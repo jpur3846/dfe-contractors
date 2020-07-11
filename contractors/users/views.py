b782967cce24c0176c49c074657479efaef253c1
Jack Purdon <jpur3846@uni.sydney.edu.au>
2020-07-11 15:15:47 +1000

    removed settings.py

--- contractors/users/views.py
+++ contractors/users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-from .forms import UserCreationForm, ContractorProfileForm
-from events.models import Event, Contractor
+from .forms import SignupForm, ContractorProfileForm
+from .models import Contractor
 
 # Default landing page.
 def user_home(request):
@@ -18,45 +19,39 @@ def signin_view(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
-        user = authenticate(request, username=email, password=password)
+        user = authenticate(request, username=email, password=password) # No username. Username and email are same.
 
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("user_home"))
         else:
             return render(request, "users/signin.html", {
-                "message": "Invalid Credentials."
+                "message": messages.error(request, 'Invalid Credentials')
             })
 
     return render(request, 'users/signin.html')
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        contractor_profile_form = ContractorProfileForm(request.POST)
+        form = SignupForm(request.POST)
+        contractor_form = ContractorProfileForm(request.POST)
 
-        if form.is_valid() and contractor_profile_form.is_valid():
+        if form.is_valid() and contractor_form.is_valid():
             user = form.save()
-            profile = contractor_profile_form.save(commit=False) # won't save to db right away.
-            profile.user = user # This hooks up our user to their contractor info.
+            contractor = contractor_form.save(commit=False)
 
-            profile.save()
-
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            first_name = request.POST['first_name'] # How do I post first name?
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            contractor.user = user
+            contractor.save()
 
             return render(request, "users/signin.html", {
                 "message": "You have successfully signed up!"
             })
         
     else:
-        form = UserCreationForm()
-        contractor_profile_form = ContractorProfileForm()
+        form = SignupForm()
+        contractor_form = ContractorProfileForm()
         
-    context = {'form': form, 'contractor_profile_form': contractor_profile_form }
+    context = {'form': form}
     return render(request, 'users/signup.html', context)
 
 def signout_view(request):
