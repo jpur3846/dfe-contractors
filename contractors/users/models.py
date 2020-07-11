7bddf3c735689853b5d433261eba97995e637222
Jack Purdon <jpur3846@uni.sydney.edu.au>
2020-07-11 22:05:30 +1000

    added default profile photo, changed user template and changed location of user model

--- contractors/users/models.py
+++ contractors/users/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Contractor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=None) # .CASCADE will delete the user account related to this contractor and vice versa.
     instrument = models.CharField(max_length=100, default=None, null=True)
+    profile_picture = models.ImageField(null=True, blank=True, default='generic-user.jpg')
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name} {self.user.username}'
